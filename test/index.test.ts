70f7346cca8f18bc5f87a58119a15dd859a35ec9
Bernd Meyer <bernd.meyer@hexagon.com>
2022-02-03 16:50:56 +0100

    Add test file

---
+++ test/index.test.ts
@@ -0,0 +1,16 @@
+import { toHtml } from '../src/index';
+
+const baseP = "C:/work/vg/vgs/components/vgs.reportingtool/reporteditor/";
+
+console.log(JSON.stringify(toHtml(
+  baseP + "package.json",
+  [baseP + "node_modules"],
+  baseP + "../../vgs.webenvironment/npm_license_config/license_files",
+  baseP + "../../vgs.webenvironment/npm_license_config/config.json",
+  baseP + "../../vgs.webenvironment/npm_license_config/template.html",
+  false,
+  {
+    redundantHomepageOverrides: "suppress",
+    redundantLicenseOverrides: "suppress"
+  }
+), null, 2));
